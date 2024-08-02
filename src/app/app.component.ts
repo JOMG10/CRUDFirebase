e720e970317bd6469264c64db9eb338ef078b78d
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-02 13:12:37 -0600

    ....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CRUDLayoutComponent} from "./components/crudlayout/crudlayout.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CRUDLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
