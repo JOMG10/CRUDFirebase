254efaa272b74de33ec2cd96d21437192cf7d368
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-02 12:41:16 -0600

    ....

---
+++ src/app/components/crudlayout/crudlayout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crudlayout',
+  standalone: true,
+  imports: [],
+  templateUrl: './crudlayout.component.html',
+  styleUrl: './crudlayout.component.css'
+})
+export class CRUDLayoutComponent {
+
+}
