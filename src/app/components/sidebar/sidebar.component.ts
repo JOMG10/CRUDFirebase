f863597d4fedd82e4ef64f2bfa47594649134fee
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-03 09:58:42 -0600

    ....

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {CrudFireBaseService} from "../../services/crud-fire-base.service";
 import 'flowbite';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
