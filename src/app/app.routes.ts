3aab0c369713d2e64165c0938c23720436e39ff3
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-03 09:58:19 -0600

    ....

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  // {
+  //   path: '',
+  //   loadComponent: () => import('./components/layout/layout.component').then(m => m.LayoutComponent),
+  //   children: [
+  //     {
+  //       path:'dashboard',
+  //       loadComponent:()=> import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  //     },
+  //     {
+  //       path:'crudlayout',
+  //       loadComponent:()=> import('./components/crudlayout/crudlayout.component').then(m => m.CRUDLayoutComponent)
+  //     },
+  //     {
+  //       path:'',
+  //       redirectTo:'dashboard',
+  //       pathMatch:'full'
+  //     }
+  //     ]
+  // },
+  // {
+  //   path:'**',
+  //   redirectTo:'dashboard'
+  // }
+];
