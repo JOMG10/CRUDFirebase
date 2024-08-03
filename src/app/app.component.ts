3f01d7ae25b1bcfa277f15aba8072a58f1d91982
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-02 20:43:50 -0600

    ....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CRUDLayoutComponent} from "./components/crudlayout/crudlayout.component";
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {NavComponent} from "./components/nav/nav.component";
+import {MainComponent} from "./components/main/main.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CRUDLayoutComponent],
+  imports: [RouterOutlet, CRUDLayoutComponent, SidebarComponent, NavComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
