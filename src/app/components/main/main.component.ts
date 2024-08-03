14422998d3412a9ec556a7ace9cfc1508efbb520
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-02 20:44:07 -0600

    ....

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {CRUDLayoutComponent} from "../crudlayout/crudlayout.component";
+import {CrudFireBaseService} from "../../services/crud-fire-base.service";
+import {NgIf} from "@angular/common";
+
+
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [
+    CRUDLayoutComponent,
+    NgIf
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent implements OnInit{
+
+
+  variable:any
+
+  constructor(private compartirDatosService: CrudFireBaseService) {}
+
+  ngOnInit() {
+    this.compartirDatosService.variable$.subscribe(valor => {
+      if (valor==="home"){
+        this.variable = "home";
+      }else if(valor==="dashboard"){
+        this.variable = "dashboard";
+
+
+      }
+
+    });
+    }
+
+
+
+}
