3c25a1ef0f9866f404fba2f2f691c8e5d85710b3
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-03 00:09:35 -0600

    ....

---
+++ src/app/components/cruddatos/cruddatos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cruddatos',
+  standalone: true,
+  imports: [],
+  templateUrl: './cruddatos.component.html',
+  styleUrl: './cruddatos.component.css'
+})
+export class CRUDDatosComponent {
+
+}
