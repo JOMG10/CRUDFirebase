c2813ad5bc3febafb89c05f7b67a9040e6a18860
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-02 20:44:32 -0600

    ....

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {CrudFireBaseService} from "../../services/crud-fire-base.service";
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  home: string="home"
+  page2="pagina2"
+  dashboard="dashboard"
+
+
+  constructor(private crudService: CrudFireBaseService) {
+  }
+
+  cambiarVariable(pagina: string){
+    this.crudService.cambiarVariable(pagina)
+  }
+
+}
