c6a8c0be9040380262abf5c2df874dbfd98ab007
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-03 00:09:41 -0600

    ....

--- src/app/components/crudlayout/crudlayout.component.ts
+++ src/app/components/crudlayout/crudlayout.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
 import 'flowbite';
+import { Directive, ElementRef, Renderer2, Input, OnChanges } from '@angular/core';
+import {NgClass} from "@angular/common";
+
 
 @Component({
   selector: 'app-crudlayout',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './crudlayout.component.html',
   styleUrl: './crudlayout.component.css'
 })
 export class CRUDLayoutComponent {
 
+
+  isModalOpen = false;
+  isModalDelete=false
+
+  ismodal=false
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  openModalDelete() {
+    this.isModalDelete = true
+  }
+
+  closeModalDelete() {
+    this.isModalDelete = false
+  }
+
+  openM() {
+    this.ismodal=true
+  }
+
+  closeM(){
+    this.ismodal=false
+  }
 }
