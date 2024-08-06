18a9c6256d70ef9e6cbccaf5bc3e1f072dbf21c3
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-05 23:59:12 -0600

    ....

--- src/app/pages/crudlayout/crudlayout.component.ts
+++ src/app/pages/crudlayout/crudlayout.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import 'flowbite';
 import { Directive, ElementRef, Renderer2, Input, OnChanges } from '@angular/core';
-import {NgClass} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {Productos} from "../../models/produtos.models";
+import {FormsModule} from "@angular/forms";
 
 
 @Component({
   selector: 'app-crudlayout',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    NgForOf,
+    FormsModule,
+    NgIf
   ],
   templateUrl: './crudlayout.component.html',
   styleUrl: './crudlayout.component.css'
@@ -21,12 +25,28 @@ export class CRUDLayoutComponent {
 
    productos: Productos[] = [];
 
+   agregarNuevo(){
+     console.log("entrando al metodo")
+     const nuevoProducto={
+       name:"jose",
+       category:"Martinez",
+       brand:"fwefew",
+       description:"string",
+       price:"string"
+     }
+     this.productos.push(nuevoProducto);
+     console.log(this.productos)
+   }
+
   openModal() {
     this.isModalOpen = true;
+    console.log("modal abierto")
   }
 
   closeModal() {
     this.isModalOpen = false;
+    console.log("modal cerrado")
+
   }
 
 }
