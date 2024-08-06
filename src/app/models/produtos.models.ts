e045988036936646660016e8429c148c85b63b15
Joseg <l20920129@voaxaca.tecnm.mx>
2024-08-05 23:59:15 -0600

    ....

--- src/app/models/produtos.models.ts
+++ src/app/models/produtos.models.ts
@@ -1,16 +1,10 @@
-export class Productos{
+export interface Productos{
   name:string;
   category:string;
   brand:string;
   description:string;
   price:string;
 
-  constructor(name:string, category:string, brand:string, description:string, price:string) {
-    this.name=name;
-    this.category = category;
-    this.brand = brand;
-    this.description = description;
-    this.price = price;
-  }
+
 }
 
